b946ac0047cdf5b685f1d8b3b3bc0e680caae564
Luvixs <148796042+Luvixs@users.noreply.github.com>
2025-05-13 17:29:55 +0200

    Es Spotify Seconda Parte
    completata la class PlayList

--- Prj_3_Spotify/model/Playlist.java
+++ Prj_3_Spotify/model/Playlist.java
@@ -1,5 +1,50 @@
 package model;
 
-public class Playlist {
+import java.util.ArrayList;
 
+
+public class Playlist {
+	
+	ArrayList<Brano> listaBrani = new ArrayList<>();
+	public String nome;
+	public double durataTotale = 0;
+	public double numeroBrani = 0;
+	
+	public Playlist (String nome) {
+		this.nome = nome;
+		
+		
+	};
+	
+	public void addBrano(Brano brano) {
+		this.listaBrani.add(brano);
+		this.numeroBrani++;
+		this.durataTotale += brano.durata;
+	}
+	
+	public void removeBrano(Brano brano) {
+		this.listaBrani.remove(brano);
+		this.numeroBrani--;
+		this.durataTotale -= brano.durata;
+	}
+	
+	public void numeroBraniPlayList () {
+		this.listaBrani.size();
+	}
+	
+	/*public void calcTotDurata () {
+		 double durataTotale = 0;
+		 for(Brano brano : this.listaBrani) {
+			 durataTotale += brano.durata;
+		 }
+		 this.durataTotale = durataTotale;
+	}*/
+	public void infoPlayList() {
+		System.out.println( "Nome: " + this.nome +" "+ "Numero Brani: " + this.numeroBrani + " " + "Durata Totale: " + this.durataTotale + "\n Brani della playlist: ");
+		for(Brano brano : this.listaBrani) {
+			brano.stampaInfo();
+		}
+		}
+		
+	
 }
--- Prj_3_Spotify/view/Main.java
+++ Prj_3_Spotify/view/Main.java
@@ -1,13 +1,24 @@
 package view;
 
-import model.Brano; 
+import model.Brano;
+import model.Playlist;
 
 
 public class Main {
 	
 	public static void main(String[] args) {
-		Brano branoScelto = new Brano("Sleep Token", "The Summoning", "alternative metal", 6.36);
-		branoScelto.play();
-		branoScelto.stampaInfo();
+		Brano branoScelto1 = new Brano("Sleep Token", "The Summoning", "alternative metal", 6.36);
+		branoScelto1.play();
+		
+		Playlist nuovaPLayList = new Playlist ("La mia Play List");
+		
+		Brano branoScelto2 = new Brano ("Halestorm", "Back From the Dead", "Rock", 3.31);
+		Brano branoScelto3 = new Brano ("Marilyn Manson", "The Beautiful People", "Metal", 3.47);
+		
+		nuovaPLayList.addBrano(branoScelto1);
+		nuovaPLayList.addBrano(branoScelto2);
+		nuovaPLayList.addBrano(branoScelto3);
+		nuovaPLayList.removeBrano(branoScelto3);
+		nuovaPLayList.infoPlayList();
 	}
 }
