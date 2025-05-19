04719152cd444b8a9463775854930fc4b8b60c8d
Luvixs <148796042+Luvixs@users.noreply.github.com>
2025-05-19 20:53:06 +0200

    inizio es registro studenti

--- Prj_4_RegistroAula/src/model/Studente.java
+++ Prj_4_RegistroAula/src/model/Studente.java
@@ -9,4 +9,38 @@ public class Studente {
  * contatore se serve
  * 
  * */
+	
+	private String nome;
+	private String cognome;
+	private int matricola;
+	private String corso;
+	
+	public Studente(String cognome, String corso, String nome, int matricola) {
+		super();
+		this.cognome = cognome;
+		this.corso = corso;
+		this.nome = nome;
+		this.matricola = matricola;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getCognome() {
+		return cognome;
+	}
+
+	public int getMatricola() {
+		return matricola;
+	}
+
+	public String getCorso() {
+		return corso;
+	}
+	
+	
+	
+	
+
 }
